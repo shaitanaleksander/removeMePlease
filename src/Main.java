201b63d612c2685f1986be2b01b57c435245adc6
shaitan <shaitanaleksender@gmail.com>
2024-11-28 19:27:04 +0200

    added printer method

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+
+    public static void printer(String name) {
+        System.out.println( "hello " + name);
+    }
 }
